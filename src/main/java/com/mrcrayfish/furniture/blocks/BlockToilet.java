888cdef4a996cfdba2dc0727f8de2b1e3cd9f8d4
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-03-05 14:52:35 +0100

    Fix farting button handling
    Courtesy of @Ocelot5836

--- src/main/java/com/mrcrayfish/furniture/blocks/BlockToilet.java
+++ src/main/java/com/mrcrayfish/furniture/blocks/BlockToilet.java
@@ -19,12 +19,11 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import org.lwjgl.input.Keyboard;
 
 import java.util.List;
 
@@ -70,8 +69,7 @@ public class BlockToilet extends BlockFurniture
             {
                 if(worldIn.isRemote)
                 {
-                    String key = new TextComponentTranslation("cfm.message.toilet", Keyboard.getKeyName(InputHandler.KEY_FART.getKeyCode())).getUnformattedComponentText();
-                    playerIn.sendMessage(new TextComponentString(TextFormatting.YELLOW + key));
+                    playerIn.sendMessage(new TextComponentTranslation("cfm.message.toilet", InputHandler.KEY_FART.getDisplayName()).setStyle(new Style().setColor(TextFormatting.YELLOW)));
                 }
                 worldIn.updateComparatorOutputLevel(pos, this);
             }
