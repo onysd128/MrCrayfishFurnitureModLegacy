21dad0f5f196a4d87b9205c3792677293f5837f2
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-05-26 11:37:09 +0200

    Update RFG, use tags

--- src/main/java/com/mrcrayfish/furniture/Reference.java
+++ src/main/java/com/mrcrayfish/furniture/Reference.java
@@ -1,10 +1,12 @@
 package com.mrcrayfish.furniture;
 
+import com.mrcrayfish.cfm.Tags;
+
 public class Reference
 {
-    public static final String MOD_ID = "cfm";
+    public static final String MOD_ID = Tags.MOD_ID;
     public static final String NAME = "MrCrayfish's Furniture Mod Legacy";
-    public static final String VERSION = "6.5.0";
+    public static final String VERSION = Tags.VERSION;
     public static final String ACCEPTED_MC_VERSIONS = "[1.12.2]";
     public static final String DEPENDENCIES = "after:biomesoplenty;after:crafttweaker";
     public static final String CLIENT_PROXY_CLASS = "com.mrcrayfish.furniture.proxy.ClientProxy";
